eadc66b55769d50a02ef27645b63b5c95fc81fa7
Mooner92 <54096383+mooner92@users.noreply.github.com>
2022-10-06 14:31:21 +0900

    add
    add

--- 역설계/Form1.cs
+++ 역설계/Form1.cs
@@ -14,28 +14,12 @@ namespace 역설계
 {
     public partial class Form1 : Form
     {
-        /*OleDbConnection conn = null;
-        OleDbCommand comm = null;
-        OleDbCommand comm2 = null;
-        OleDbCommand comm3 = null;
-        OleDbDataReader reader = null;
-
-        string connStr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\82105\Desktop\SoilMoisture1.accdb";
-        string Time = DateTime.Now.ToString();
-        string Moisture = "55%";*/
-
+        
         public Form1()
         {
             
             InitializeComponent();
-            /*
-            string query = "INSERT INTO SoilSensorDB(SenSorNum, SDate, Time, SenSorValue, Moisture) VALUES (0, '"+ Time +"', '"+sv1+"', '"+Moisture + "')";
-
-            conn = new OleDbConnection(connStr);
-            conn.Open();
-            comm3 = new OleDbCommand(query, conn);
-            comm3.ExecuteNonQuery();
-            */
+            
         }
 
         private void viewToolStripMenuItem_Click(object sender, EventArgs e)
